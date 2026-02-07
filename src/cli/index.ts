bb8788dce02021cdfa6be72022aee73622ae1e5a
LogIN- <info@ivantomic.com>
2026-02-07 11:58:52 -0500

    [feature] Website / Bio

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -8,6 +8,17 @@ import { inspectCommand } from './commands/inspect';
 import { bioCommand } from './commands/bio';
 import { logger } from '../utils/logger';
 
+// Polyfills for pdf-parse (pdf.js dependency) which crashes in Node 18+ without these
+if (typeof global.DOMMatrix === 'undefined') {
+    (global as any).DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.ImageData === 'undefined') {
+    (global as any).ImageData = class ImageData {};
+}
+if (typeof global.Path2D === 'undefined') {
+    (global as any).Path2D = class Path2D {};
+}
+
 const program = new Command();
 
 program
